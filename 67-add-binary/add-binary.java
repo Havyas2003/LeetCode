d8f88ab16c04b38ab9e8ba88ce632e338eb98258
Havyas-Poojary <poojaryhavyas@gmail.com>
2025-07-27 12:34:31 +0530

    Time: 1 ms (99.83%) | Memory: 42.2 MB (81.13%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,15 +4,8 @@ class Solution {
         int i = a.length() - 1, j = b.length() - 1, carry = 0;
 
         while (i >= 0 || j >= 0 || carry != 0) {
-            int digitA = 0;
-if (i >= 0) {
-    digitA = a.charAt(i) - '0';
-}
-
-int digitB = 0;
-if (j >= 0) {
-    digitB = b.charAt(j) - '0';
-}
+            int digitA = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int digitB = (j >= 0) ? b.charAt(j) - '0' : 0;
 
             int sum = digitA + digitB + carry;
             result.append(sum % 2);       // current bit
