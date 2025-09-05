104d24f9f4a2337c99fadf6d889eb694fcce6aaf
Havyas-Poojary <poojaryhavyas@gmail.com>
2025-09-05 23:24:37 +0530

    Time: 2 ms (47.91%) | Memory: 12.6 MB (19.85%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -9,13 +9,15 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if not head:
+        if not head: #check if the head is empty i.e list is empty
             return head
-        c=head
-        while c and c.next:
-            if c.val==c.next.val:
-                c.next=c.next.next
-            else:
-                c=c.next
-        return head
+        c=head #taking a current variable initate to head
+        while c and c.next: #loops only when c and next element of c is not None
+            if c.val==c.next.val: #if value of 2 equal
+                c.next=c.next.next #we skip the next element
+            else: #the values aren't equal 
+                c=c.next #we simply traverse it 
+        return head #returning hte head of the list after removing duplicates
+        
+        
         
